f6b3daf9f607cf57fa8b7d2e6c8d4e9f953b7080
ghostandthemachine <jonrose08@gmail.com>
2013-10-27 18:28:04 -0700

    binary string to mapped binary values

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject roboto "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "CSU CSCI_580 Homework assignment. Robot in grid world problem."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
--- src/roboto/core.clj
+++ src/roboto/core.clj
@@ -4,25 +4,25 @@
   (:require [clojure.pprint]))
 
 ;; Initial R Matrix
-(def R (array [[0       (/ 1 2) 0       0     0       (/ 1 2) 0       0       0       0       0       0       0      ]
-               [(/ 1 2) 0       (/ 1 3) 0     0       0       0       0       0       0       0       0       0      ]
-               [0       (/ 1 2) 0       (/ 1 2) 0     0       (/ 1 2) 0       0       0       0       0       0      ]
-               [0       0       (/ 1 3) 0     (/ 1 2) 0       0       0       0       0       0       0       0      ]
-               [0       0       0       (/ 1 2) 0     0       0       (/ 1 2) 0       0       0       0       0      ]
-               [(/ 1 2) 0       0       0     0       0       0       0       (/ 1 2) 0       0       0       0      ]
-               [0       0       (/ 1 3) 0     0       0       0       0       0       0       (/ 1 3) 0       0      ]
-               [0       0       0       0     (/ 1 2) 0       0       0       0       0       0       0       (/ 1 2)]
-               [0       0       0       0     0       (/ 1 2) 0       0       0       (/ 1 2) 0       0       0      ]
-               [0       0       0       0     0       0       0       0       (/ 1 2) 0       (/ 1 3) 0       0      ]
-               [0       0       0       0     0       0       (/ 1 2) 0       0       (/ 1 2) 0       (/ 1 2) 0      ]
-               [0       0       0       0     0       0       0       0       0       0       (/ 1 3) 0       (/ 1 2)]
-               [0       0       0       0     0       0       0       (/ 1 2) 0       0       0       (/ 1 2) 0      ]]))
+(def R (array [[0       (/ 1 2) 0       0       0       (/ 1 2) 0       0       0       0       0       0       0      ]
+               [(/ 1 2) 0       (/ 1 3) 0       0       0       0       0       0       0       0       0       0      ]
+               [0       (/ 1 2) 0       (/ 1 2) 0       0       (/ 1 2) 0       0       0       0       0       0      ]
+               [0       0       (/ 1 3) 0       (/ 1 2) 0       0       0       0       0       0       0       0      ]
+               [0       0       0       (/ 1 2) 0       0       0       (/ 1 2) 0       0       0       0       0      ]
+               [(/ 1 2) 0       0       0       0       0       0       0       (/ 1 2) 0       0       0       0      ]
+               [0       0       (/ 1 3) 0       0       0       0       0       0       0       (/ 1 3) 0       0      ]
+               [0       0       0       0       (/ 1 2) 0       0       0       0       0       0       0       (/ 1 2)]
+               [0       0       0       0       0       (/ 1 2) 0       0       0       (/ 1 2) 0       0       0      ]
+               [0       0       0       0       0       0       0       0       (/ 1 2) 0       (/ 1 3) 0       0      ]
+               [0       0       0       0       0       0       (/ 1 2) 0       0       (/ 1 2) 0       (/ 1 2) 0      ]
+               [0       0       0       0       0       0       0       0       0       0       (/ 1 3) 0       (/ 1 2)]
+               [0       0       0       0       0       0       0       (/ 1 2) 0       0       0       (/ 1 2) 0      ]]))
 
 ;; Initial F vector
 (def F (array (reduce (fn [r _] (conj r (/ 1 13))) [] (range 13))))
 
 ;; World boundary masks based on given initial world
-(def WORLD_BOUNDRIES ["1010" "1100" "1000" "1100" "1001" "0011" "0011" "0011" "0110" "1100" "0100" "1100" "0101"])
+(def WORLD_BOUNDRIES (map str [1010 1100 1000 1100 1001 0011 0011 0011 0110 1100 0100 1100 0101]))
 
 ;; Predefined discrepancies
 (def DISCREPANCIES [0.6561 0.0729 0.0081 0.0009 0.0001])
@@ -65,14 +65,13 @@
       (assoc results :indexes (into [] (find-max-indexes (:F results))))))
 
 (defn -main
-  "Main application function to calculate Robot world"
+  "Main application function to calculate robot's position in grid world."
   [& args]
   (let [num     (read-string (get-user-input "Enter number of iterations to calculate:"))
-        _       (println)
-        _       (println (str "Calculating with <" num "> iterations"))
-        _       (println)
+        _       (do (println) (println (str "Calculating with <" num "> iterations")) (println))
         data    (calculate num)
         indexes (:indexes data)]
     (clojure.pprint/pprint data)
     (println)
-    (println (str "Most likely position" (if (> (count indexes) 1) "s are: " " is: ") indexes))))
+    (println (str "Most likely position" (if (> (count indexes) 1) "s are: " " is: ") indexes))
+    (println)))
